515f894bdcc797afd6c22abf0ea29523ce03990e
Vlad Vinskevitch <vvinskevitch@gmail.com>
2021-06-03 10:24:57 +0300

    first commit

---
+++ src/main/java/com/sharkit/maxup/MaxupApplication.java
@@ -0,0 +1,13 @@
+package com.sharkit.maxup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharkit/maxup/MaxupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharkit.maxup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
